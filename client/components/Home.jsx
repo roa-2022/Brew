049c20a5519d1664b55325cb31372f852dc40d94
Brendan Jarvis <jarvisbrendanj@gmail.com>
2022-09-18 21:34:36 +1200

    Add carousel to home.jsx

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,7 +1,49 @@
 import React from 'react'
 
+import { Carousel, Container } from 'react-bootstrap/'
+
 function Home() {
-  return <h1>This is a home screen, leave me alone Router.</h1>
+  return (
+    <Container fluid>
+      <Carousel fade>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/lutz-wernitz-pcW5bR7gSJ4-unsplash.jpg"
+            alt="Light malt grains"
+          />
+          <Carousel.Caption>
+            <h3>Malt</h3>
+            <p>Fementables.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="markus-spiske-qn5iDwvOZgo-unsplash.jpg"
+            alt="Hop flowers"
+          />
+
+          <Carousel.Caption>
+            <h3>Hops</h3>
+            <p>Flavour/Bittering.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/adam-wilson-I2f5BbeXPVY-unsplash.jpg"
+            alt="Bottled homebrew beers"
+          />
+
+          <Carousel.Caption>
+            <h3>Beer</h3>
+            <p>Brew!</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </Container>
+  )
 }
 
 export default Home
